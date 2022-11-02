f9cdf281017c19e6a02742667c59a2aed2a32925
Taylor Allen <taylorallen0913@gmail.com>
2022-11-01 17:42:17 -0700

    test

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 names = list(input().split())
 
-print(min(names), max(names))
+min_name = names[0]
+max_name = names[0]
+
+for name in names:
+    if name < min_name:
+        min_name = name
+    if name > max_name:
+        max_name = name
+
+print(min_name, max_name)
